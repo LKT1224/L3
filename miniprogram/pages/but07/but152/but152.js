b0798b6585f16f6030b2c96672a4e614e890fcb0
LKT1224 <59135452+LKT1224@users.noreply.github.com>
2019-12-22 20:31:22 +0800

    Add files via upload

---
+++ miniprogram/pages/but07/but152/but152.js
@@ -0,0 +1,23 @@
+// pages/but/but152/but152.js
+Component({
+  /**
+   * 组件的属性列表
+   */
+  properties: {
+
+  },
+
+  /**
+   * 组件的初始数据
+   */
+  data: {
+
+  },
+
+  /**
+   * 组件的方法列表
+   */
+  methods: {
+
+  }
+})
